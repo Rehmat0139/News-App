6dbe274aa1787cde7c0ed84485d7abe47869946b
rehmat <rehmat0139@gmail.com>
2023-04-27 13:58:27 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ export class App extends Component {
         <Router>
           <Navbar />
           <Switch>
-            <Route exact path="/News-App/"><News key='general' pageSize={this.pageSize} country='in' category='general' /></Route>
-            <Route exact path="/News-App/business"><News key='business' pageSize={this.pageSize} country='in' category='business' /></Route>
-            <Route exact path="/News-App/entertainment"><News key='entertainment' pageSize={this.pageSize} country='in' category='entertainment' /></Route>
-            <Route exact path="/News-App/health"><News key='health' pageSize={this.pageSize} country='in' category='health' /></Route>
-            <Route exact path="/News-App/science"><News key='science' pageSize={this.pageSize} country='in' category='science' /></Route>
-            <Route exact path="/News-App/sports"><News key='sports' pageSize={this.pageSize} country='in' category='sports' /></Route>
-            <Route exact path="/News-App/technology"><News key='technology' pageSize={this.pageSize} country='in' category='technology' /></Route>
+            <Route exact path="/News-app"><News key='general' pageSize={this.pageSize} country='in' category='general' /></Route>
+            <Route exact path="/business"><News key='business' pageSize={this.pageSize} country='in' category='business' /></Route>
+            <Route exact path="/entertainment"><News key='entertainment' pageSize={this.pageSize} country='in' category='entertainment' /></Route>
+            <Route exact path="/health"><News key='health' pageSize={this.pageSize} country='in' category='health' /></Route>
+            <Route exact path="/science"><News key='science' pageSize={this.pageSize} country='in' category='science' /></Route>
+            <Route exact path="/sports"><News key='sports' pageSize={this.pageSize} country='in' category='sports' /></Route>
+            <Route exact path="/technology"><News key='technology' pageSize={this.pageSize} country='in' category='technology' /></Route>
           </Switch>
         </Router>
       </div>
